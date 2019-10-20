245d4fbffac7987d87664860997b82f593835c28
Crinze20 <34103141+Crinze20@users.noreply.github.com>
2019-10-20 16:31:23 +0700

    In Memory

--- MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductCategoryController.cs
@@ -12,11 +12,11 @@ namespace MyShop.WebUI.Controllers
     {
 
 
-        ProductCategoryRepository context;
+        InMemoryRepository<ProductCategory> context;
 
         public ProductCategoryController()
         {
-            context = new ProductCategoryRepository();
+            context = new InMemoryRepository<ProductCategory>();
         }
 
         // GET: Product
--- MyShop/MyShop.WebUI/Controllers/ProductController.cs
+++ MyShop/MyShop.WebUI/Controllers/ProductController.cs
@@ -12,13 +12,13 @@ namespace MyShop.WebUI.Controllers
     public class ProductController : Controller
     {
 
-        ProductRepository context;
-        ProductCategoryRepository productCategories;
+        InMemoryRepository<Product> context;
+        InMemoryRepository<ProductCategory> productCategories;
 
         public ProductController()
         {
-            context = new ProductRepository();
-            productCategories = new ProductCategoryRepository();
+            context = new InMemoryRepository<Product>();
+            productCategories = new InMemoryRepository<ProductCategory>();
         }
 
         // GET: Product
