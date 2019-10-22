1bcb8f69d0935405c2739c4bc271624944b6ec0c
BRI <christ_natalius@outlook.com>
2019-10-22 14:50:05 +0700

    Commit, untuk Entity Framework

---
+++ MyShop/MyShop.WebUI/Keterangan.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyShop.WebUI
+{
+    public class Keterangan
+    {
+
+        /*
+         
+        [22 okt 2019]
+        nanti dirumah, cukup ikutin perintah 5 aja.
+
+
+
+
+         Proses untuk install entity framework :
+
+        1. Install Entity Framework pake nuget, biasanya 6.0.0 , (kalau 6.3.0 ga bisa Enable-Migrations)
+        2. Open Package Manager Console.
+        3. Ketik : Enable-Migrations
+            - Untuk membuat folder migration, untuk siap dieksekusi
+        4. Ketik : Add-Migration Initial.
+            - Memberikan inisal db ke dalam folder migration
+        5. Ketik : Update-Database 
+            - Ini bisa mengupdate ke database benaran sesuai servernya, jadi misalnya belum ada dbnya, nanti akan terupdate
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         
+         */
+    }
+}
